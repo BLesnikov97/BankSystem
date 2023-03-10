0c0f26e17cc593e231011f9854fc40f6d08f65bf
lesnikovbs <lesnikovbs@gmail.com>
2023-03-10 13:30:53 +0300

    update_

--- BankSystem.Client.WPF/UI/EditAccount/EditAccountViewModel.cs
+++ BankSystem.Client.WPF/UI/EditAccount/EditAccountViewModel.cs
@@ -1,6 +1,121 @@
-﻿namespace BankSystem.Client.WPF.UI.EditAccount
+﻿using BankSystem.BusinesLogic.Repositories;
+using BankSystem.BusinesLogic.Services;
+using BankSystem.BusinessLogic.Model;
+using BankSystem.Client.WPF.Util;
+using System.Collections.Generic;
+
+namespace BankSystem.Client.WPF.UI.EditAccount
 {
-    public class EditAccountViewModel
+    public class EditAccountViewModel : BaseViewModel
     {
+        private List<User> _users;
+
+        private List<Account> _accounts;
+
+        private Account _selectedAccount;
+
+        private User _selectedUser;
+
+        private string _description;
+
+        private double _amount;
+
+        private string _currency;
+
+        private IService _service;
+
+        private IRepository _db;
+
+        public EditAccountViewModel(IService serviceRepository, IRepository repository)
+        {
+            _service = serviceRepository;
+            _db = repository;
+            _users = _db.GetUsersList();
+        }
+
+
+        private RelayCommand editAccount;
+
+        public RelayCommand EditAccount
+        {
+            get
+            {
+                return editAccount ??
+                    (editAccount = new RelayCommand(obj =>
+                    {
+
+                    }));
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+            set
+            {
+                _amount = value;
+                OnPropertyChanged("Amount");
+            }
+        }
+
+        public string Currency
+        {
+            get { return _currency; }
+            set
+            {
+                _currency = value;
+                OnPropertyChanged("Currency");
+            }
+        }
+
+        public List<User> Users
+        {
+            get { return _users; }
+            set
+            {
+                _users = value;
+                OnPropertyChanged("Users");
+            }
+        }
+
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+
+        public List<Account> Accounts
+        {
+            get { return _accounts; }
+            set
+            {
+                _accounts = value;
+                OnPropertyChanged("Accounts");
+            }
+        }
+
+        public Account SelectedAccount
+        {
+            get { return _selectedAccount; }
+            set
+            {
+                _selectedAccount = value;
+                OnPropertyChanged("SelectedAccount");
+            }
+        }
     }
 }
